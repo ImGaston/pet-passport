12ef441e74937d62b01236ecbf56d3a6e72cc061
Gaston <corbalan.gaston@outlook.com>
2023-06-14 00:09:45 -0300

    initial commit

---
+++ src/app/astor/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Astor = () => {
+  return (
+    <main>
+      <h1>Astor</h1>
+    </main>
+  );
+};
+
+export default Astor;
---
+++ src/app/roma/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Roma = () => {
+  return (
+    <main>
+      <h1>Roma</h1>
+    </main>
+  );
+};
+
+export default Roma;
