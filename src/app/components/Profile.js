0439c90cc116933da65866e0fcdca5af884a957f
Gaston <corbalan.gaston@outlook.com>
2023-06-19 12:15:51 -0300

    image styled

--- src/app/components/Profile.js
+++ src/app/components/Profile.js
@@ -1,8 +1,12 @@
 import Image from 'next/image';
 import React from 'react';
 
+const imageStyle = {
+  borderRadius: '0.75rem',
+};
+
 const Profile = ({ picture }) => {
-  return <Image src={picture} width={200} height={200} alt='pet picture' />;
+  return <Image src={picture} width={200} height={200} alt='pet picture' style={imageStyle} />;
 };
 
 export default Profile;
